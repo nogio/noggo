1611d2d67a7d33e761de1bc2e34bc87b479d1b7e
jorise <jorise@qq.com>
2016-09-26 16:24:45 +0800

    初始化测试

--- exit.go
+++ exit.go
@@ -1,5 +1,22 @@
 package noggo
 
-func Exit() {
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"fmt"
+)
 
+func exitWaiting() {
+	//使用管道监听退出信号
+	exitChan := make(chan os.Signal, 1)
+	signal.Notify(exitChan, os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM)
+	<-exitChan
+}
+
+func Exit() {
+	exitWaiting()
+
+	//退出前处理
+	fmt.Println("noggo exit...")
 }
